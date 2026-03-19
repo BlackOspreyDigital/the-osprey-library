6b365f416b27b974a3047cdf9cea7dc5c8e35b06
BlackOspreyDigital <noizeindex@gmail.com>
2026-03-19 07:32:43 -0500

    cloudwrk updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,8 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://theospreylibrary.com',
   integrations: [mdx(), sitemap()],
-  adapter: cloudflare({
-    imageService: 'passthrough', // <-- Images were working just needed to pass
-  }),
 });
